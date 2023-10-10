11c9d44d7f68cd15edaf24b4cf96dfb418a18830
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-10 09:03:07 -0500

    lint formatting

--- src/handlers/workflows/local/node-project/lib/search-for-index.mjs
+++ src/handlers/workflows/local/node-project/lib/search-for-index.mjs
@@ -3,7 +3,7 @@ import createError from 'http-errors'
 const searchForIndex = (files) => {
   const indexes = files.filter((f) => f.name.match(/^index\.[mc]?js$/))
   if (indexes.length > 1) {
-    throw createError.BadRequest('Multiple index files found, bailing out: ' 
+    throw createError.BadRequest('Multiple index files found, bailing out: '
       + indexes.map(({ name }) => name).join(', '))
   }
   const index = indexes[0]
