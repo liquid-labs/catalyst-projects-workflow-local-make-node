d9daa39f328e2bbf7fe210b7bb5067e6f4f701c7
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-31 10:08:09 -0500

    update name to workflow-local-make-node

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,6 @@
 export * from './handlers'
 
-const name = 'builder-node'
+const name = 'workflow-local-make-node'
 const summary = 'Creates standard build, test, and lint Makefile targets for node components.'
 
 export { name, summary }
