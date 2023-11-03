f8f65137bcb72e236b9bf5897291b6255bcbada5
Zane Rockenbaugh <zane@liquid-labs.com>
2023-11-03 10:35:33 -0500

    parameter summary -> description

--- src/handlers/workflows/local/node-project/setup-standard.mjs
+++ src/handlers/workflows/local/node-project/setup-standard.mjs
@@ -5,7 +5,6 @@ import { setupProject } from './lib/setup-project'
 
 const help = {
   name        : 'Setup node project',
-  summary     : 'Sets up make based build, test, and lint for node projects.',
   description : `By default, this method will analyze the current project and produce an appropriate 'Makefile' with build, test, and lint options and also install necessary 'devDependencies'. The analysis is as follows:
 
   1. If there is an 'index.js' (or '.mjs', '.cjs') file in the source directory (generally 'src'), then we assume the package to define a single library whose output will be determined by the 'main' field in 'package.json'.
@@ -17,63 +16,62 @@ const method = 'put'
 const path = ['workflows', 'local', 'node-project', 'setup-standard']
 const parameters = [
   {
-    name    : 'distPath',
-    summary : "Defines the package root relative distribution directory. Defaults to settings value 'dist'."
+    name        : 'distPath',
+    description : "Defines the package root relative distribution directory. Defaults to settings value 'dist'."
   },
   {
-    name    : 'docBuildPath',
-    summary : 'Package root relative path to the built docs.'
+    name        : 'docBuildPath',
+    description : 'Package root relative path to the built docs.'
   },
   {
-    name    : 'docSrcPath',
-    summary : 'src relative path to the documentation source.'
+    name        : 'docSrcPath',
+    description : 'src relative path to the documentation source.'
   },
   {
-    name      : 'isExecutable',
-    isBoolean : true,
-    summary   : "Applies the default build logic except that a single entry file under the source directory is treated as the source for an executable rather than a library. This setting is ignored if 'withExecutables' or 'withLibs' is specified."
+    name        : 'isExecutable',
+    isBoolean   : true,
+    description : "Applies the default build logic except that a single entry file under the source directory is treated as the source for an executable rather than a library. This setting is ignored if 'withExecutables' or 'withLibs' is specified."
   },
   {
-    name      : 'noDevInstall',
-    isBoolean : true,
-    summary   : 'If true, supresses the default behavior of looking for local development packages of resource packages to install. In other words, will always use the latest published package.'
+    name        : 'noDevInstall',
+    isBoolean   : true,
+    description : 'If true, supresses the default behavior of looking for local development packages of resource packages to install. In other words, will always use the latest published package.'
   },
   {
-    name      : 'noDoc',
-    isBoolean : true,
-    summary   : "Excludes 'doc' target from the generated makefiles."
+    name        : 'noDoc',
+    isBoolean   : true,
+    description : "Excludes 'doc' target from the generated makefiles."
   },
   {
-    name      : 'noInstall',
-    isBoolean : true,
-    summary   : "Does not install build, test, lint, etc. resources which may be needed by the scripts. Can be useful when you have set up the depencies on a specific version and don't want to override that."
+    name        : 'noInstall',
+    isBoolean   : true,
+    description : "Does not install build, test, lint, etc. resources which may be needed by the scripts. Can be useful when you have set up the depencies on a specific version and don't want to override that."
   },
   {
-    name      : 'noLint',
-    isBoolean : true,
-    summary   : "Excludes 'lint' and 'lint-fix' targets from the generated makefiles."
+    name        : 'noLint',
+    isBoolean   : true,
+    description : "Excludes 'lint' and 'lint-fix' targets from the generated makefiles."
   },
   {
-    name      : 'noTest',
-    isBoolean : true,
-    summary   : "Excludes 'test' targets from the generated makefiles."
+    name        : 'noTest',
+    isBoolean   : true,
+    description : "Excludes 'test' targets from the generated makefiles."
   },
   {
-    name    : 'srcPath',
-    summary : "Defines the package root relative source directory. Defaults to setting value or 'src'."
+    name        : 'srcPath',
+    description : "Defines the package root relative source directory. Defaults to setting value or 'src'."
   },
   {
-    name    : 'testStagingPath',
-    summary : "Defines the package root relative test staging directory. Defaults to settings value 'test-staging'."
+    name        : 'testStagingPath',
+    description : "Defines the package root relative test staging directory. Defaults to settings value 'test-staging'."
   },
   {
-    name    : 'qaPath',
-    summary : "Defines the package root relative qa directory. Defaults to settings value 'qa'."
+    name        : 'qaPath',
+    description : "Defines the package root relative qa directory. Defaults to settings value 'qa'."
   },
   {
     name         : 'withExecutables',
     isMultivalue : true,
-    summary      : "Specifies an executable to build in the form of '<entry file>:<exec file>'.",
     description  : `Each use of the parameter specifies an executable entry path and compiled executable output file relative to the effective src and dist directories respectively. E.g., with default src and dist values, 'exec-index.mjs:subdir/exec.js' would mean compilation would start with '~/src/exec-index.mjs' and produce an output executable at '~/dist/subdir/exec.js'.
 
     When 'withExecutables' is specified, the default auto-guess behavior is suppressed and all build inputs and artifacts must be fully defined.`
@@ -81,7 +79,6 @@ const parameters = [
   {
     name         : 'withLibs',
     isMultivalue : true,
-    summary      : "Specifies a lib to build in the form of '<entry file>:<lib file>'.",
     description  : `Each use of the parameter specifies a libray entry path and compiled library output file relative to the effective src and dist directories respectively. E.g., with default src and dist values, 'special-index.mjs:subdir/special-lib.js' would mean compilation would start with '~/src/special-index.mjs' and produce an output library at '~/dist/subdir/special-lib.js'.
 
     When 'withLibs' is specified, the default auto-guess behavior is suppressed and all build inputs and artifacts must be fully defined.`
